9d6f73dec7eb6399446331e6b697ece3ccd71e1b
Vivek Choudhary <vickchamp2235@gmail.com>
2023-06-18 14:45:30 +0530

    Frontend Code

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <DashBoard/>
+</template>
+
+<script>
+// import LoginBank from './components/LoginBank.vue'
+import DashBoard from './components/DashBoard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DashBoard
+  }
+}
+</script>
