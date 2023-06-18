46d78b9791090557ad52fc4741cd46404185d3df
Vivek Choudhary <vickchamp2235@gmail.com>
2023-06-18 14:46:51 +0530

    Frontend Code

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,91 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-0 py-3">
+  <div class="container-xl">
+    <!-- Logo -->
+    <ul class="navbar-brand">
+        <li class="alttext"><a href="#"><img src="@/assets/bgbankico.png"></a>&nbsp;BankApplication</li>
+    </ul>
+    <!-- Navbar toggle -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Collapse -->
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <!-- Nav -->
+      <div class="navbar-nav mx-lg-auto">
+        <a class="nav-item nav-link" href="#">Home</a>
+        <a class="nav-item nav-link" href="#">About</a>
+      </div>
+      <!-- Right navigation -->
+      <div class="navbar-nav ms-lg-4">
+        <a class="nav-item nav-link" href="#">Logout</a>
+      </div>
+      <!-- Action -->
+      <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
+        <a href="#" class="btn btn-sm btn-primary w-full w-lg-auto">
+          Register
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="row justify-content-center">
+    <div class="col-auto">
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+        <th scope="col">CustID</th>
+        <th scope="col">AccId</th>
+        <th scope="col">Name</th>
+        <th scope="col">City</th>
+        <th scope="col">Address</th>
+        <th scope="col">DOB</th>
+        <th scope="col">Open Balance</th>
+        <th scope="col">Mobile No</th>
+        <th scope="col">A Status</th>
+        <th scope="col">A Type</th>
+        <th scope="col">Occupation</th>
+        <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+        </tr>
+        </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script></script>
+
+<style>
+html, body {
+    max-width: 100%;
+    overflow-x: hidden;
+}
+ul {
+    list-style-type: none;
+}
+li{
+    font-size: larger;
+}
+table{
+    margin-top: 30px;
+}
+</style>
---
+++ src/components/LoginBank.vue
@@ -0,0 +1,117 @@
+<template>
+    <div id="login">
+    <div class="login-page">
+      <transition name="fade">
+         <div v-if="!registerActive" class="wallpaper-login"></div>
+      </transition>
+      <div class="wallpaper-register"></div>
+
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+               <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
+                  <h1>Login</h1>
+                  <form class="form-group">
+                     <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
+                     <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password" required>
+                     <div class="col-md-12 text-center mb-3">
+                        <button type="submit" class="btn btn-outline-primary mb-2" style="width: 100%;" @click="doLogin">Submit</button>
+                        <button type="reset" class="btn btn-outline-primary" style="width: 100%">Reset</button>
+                    </div>
+                  </form>
+               </div>
+
+               <div v-else class="card register" v-bind:class="{ error: emptyFields }">
+                  <!-- <h1>Sign Up</h1>
+                  <form class="form-group">
+                     <input v-model="emailReg" type="email" class="form-control" placeholder="Email" required>
+                     <input v-model="passwordReg" type="password" class="form-control" placeholder="Password" required>
+                     <input v-model="confirmReg" type="password" class="form-control" placeholder="Confirm Password" required>
+                     <input type="submit" class="btn btn-primary" @click="doRegister">
+                     <p>Already have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign in here</a>
+                     </p>
+                  </form> -->
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   </div>
+</template>
+
+<script>
+</script>
+
+<style>
+#login {
+  background: url("../assets/bgbank.jpg")  no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+p {
+   color: white;
+   line-height: 1rem;
+}
+.card {
+   padding: 20px;
+   background-color:rgba(0,0,0,0) !important;
+   box-shadow: 0 0 1rem 0 rgba(0,0,0,0.9);
+}
+
+.form-group {
+   input {
+      margin-bottom: 20px;
+   }
+}
+
+.form-control {
+    margin-bottom: 20px;
+    background: transparent !important;
+    color: white !important;
+}
+
+.login-page {
+   align-items: center;
+   display: flex;
+   height: 100vh;   
+   
+   .fade-enter-active,
+   .fade-leave-active {
+  transition: opacity .5s;
+}
+   .fade-enter,
+   .fade-leave-to {
+      opacity: 0;
+   }
+}
+
+h1{
+    color: white !important;
+    text-align: center;
+}
+
+.error {
+   animation-name: errorShake;
+   animation-duration: 0.3s;
+}
+
+@keyframes errorShake {
+   0% {
+      transform: translateX(-25px);
+   }
+   25% {
+      transform: translateX(25px);
+   }
+   50% {
+      transform: translateX(-25px);
+   }
+   75% {
+      transform: translateX(25px);
+   }
+   100% {
+      transform: translateX(0);
+   } 
+}
+</style>
